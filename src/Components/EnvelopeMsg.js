18c01ddf9cdf9baf00624f48a222b69140e5f678
arvel <franciscoarvel123@gmail.com>
2023-12-31 19:57:08 +0800

    wiht emojies

--- src/Components/EnvelopeMsg.js
+++ src/Components/EnvelopeMsg.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import "./Envelope.css"; // Import your CSS file
 import rose from "../image/back.jpg";
 import MyResponse from "./MyResponse";
+import smile from "../image/wired-lineal-261-emoji-smile.gif";
+import sad from "../image/icons8-sad (1).gif";
 function EnvelopeMsg() {
   const [isFlapOpen, setFlapOpen] = useState(false);
   const [agree, setAgree] = useState("");
   const [showResponse, setShowResponse] = useState(false);
   const [showNote, setShowNote] = useState(true);
+  const [emojeType, setEmojeType] = useState("");
   const toggleFlap = () => {
     setShowNote(!showNote);
     setFlapOpen(!isFlapOpen);
@@ -17,6 +20,10 @@ function EnvelopeMsg() {
   };
   const handleClose = () => {
     setShowResponse(!showResponse);
+  };
+
+  const hanldeMouseEnter = (text) => {
+    setEmojeType(text);
   };
   return (
     <>
@@ -56,18 +63,22 @@ function EnvelopeMsg() {
                       ug lain, but naa man gani sultie lang ko daan haha. Pili
                       lang sa ubos if you choose other or not. That's all Rose
                       thank you for being part of my 2023 bisan bag-o pata
-                      nagka-close.<strong> THANK YOU SO MUCH</strong>.
+                      nagka-close. I pray that on the years nga mo agi is part
+                      lang gihapon ka sa akong kinabuhi.
+                      <strong> THANK YOU SO MUCH</strong>.
                     </p>
                     <div className="btn-containers">
                       <button
                         className="btns other"
                         onClick={() => handleShowResponse("Not Agree")}
+                        onMouseEnter={() => hanldeMouseEnter("other")}
                       >
                         I will choose other
                       </button>
                       <button
                         className="btns only"
                         onClick={() => handleShowResponse("Agree")}
+                        onMouseEnter={() => hanldeMouseEnter("you")}
                       >
                         I will choose you
                       </button>
@@ -79,6 +90,15 @@ function EnvelopeMsg() {
 
             <div className="heart" onClick={toggleFlap}></div>
           </div>
+        </div>
+        <div className="emojies">
+          {emojeType === "you" ? (
+            <img src={smile} alt="emojie" />
+          ) : emojeType === "other" ? (
+            <img src={sad} alt="emojie" />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </>
