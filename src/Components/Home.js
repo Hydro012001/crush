d695c67a0cb1638abdb08ab8cf03fb96555d4e6e
arvel <franciscoarvel123@gmail.com>
2024-01-02 00:44:24 +0800

    failure

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,24 +8,24 @@ import MyGift from "../image/wired-lineal-412-gift.gif";
 import ToastMsg from "./ToastMsg";
 import Feedback from "./Feedback";
 import { useNavigate } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./Firebase";
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from "./Firebase";
 function Home(props) {
   const navigate = useNavigate();
-  const [response, setResponse] = useState([]);
+  //  const [response, setResponse] = useState([]);
   const [rotation, setRotation] = useState({ x: 0, y: 0 });
   const [showToast, setShowToast] = useState(true);
   const [showMessae, setShowMessage] = useState(false);
 
-  useEffect(() => {
-    async function ResponseData() {
-      const querySnap = await getDocs(collection(db, "response"));
-      const newData = querySnap.docs.map((docs) => docs.data());
-      setResponse((prevResponse) => [...prevResponse, ...newData]);
-    }
+  // useEffect(() => {
+  //   async function ResponseData() {
+  //     const querySnap = await getDocs(collection(db, "response"));
+  //     const newData = querySnap.docs.map((docs) => docs.data());
+  //     setResponse((prevResponse) => [...prevResponse, ...newData]);
+  //   }
 
-    ResponseData();
-  }, []);
+  //   ResponseData();
+  // }, []);
 
   const handleMouseMove = (event) => {
     const { clientX, clientY } = event;
@@ -61,23 +61,25 @@ function Home(props) {
   };
 
   const handleGoToSlider = () => {
-    const currentDate = new Date();
-    const newYearDate = new Date("January 1, 2024");
-    const endDate = new Date("January 2, 2024");
-    if (
-      currentDate.getTime() >= newYearDate.getTime() &&
-      currentDate.getTime() <= endDate.getTime()
-    ) {
-      navigate("slider");
-    } else if (currentDate.getTime() >= endDate.getTime()) {
-      if (response.length > 0) {
-        navigate("slider");
-      } else {
-        alert(
-          "Sorry to inform that you can no longer access this page. You have failed to open on January 1, 2024"
-        );
-      }
-    }
+    // const currentDate = new Date();
+    // const newYearDate = new Date("January 1, 2024");
+    // const endDate = new Date("January 2, 2024");
+    // if (
+    //   currentDate.getTime() >= newYearDate.getTime() &&
+    //   currentDate.getTime() <= endDate.getTime()
+    // ) {
+    //   navigate("slider");
+    // } else if (currentDate.getTime() >= endDate.getTime()) {
+    //   if (response.length > 0) {
+    //     navigate("slider");
+    //   } else {
+    //     alert(
+    //       "Sorry to inform that you can no longer access this page. You have failed to open on January 1, 2024"
+    //     );
+    //   }
+
+    // }
+    navigate("slider");
   };
   return (
     <div className="contianer">
